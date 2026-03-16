79daf477e8bc94a849f8d1eb9a2b4261427fe3b5
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-03-16 13:21:14 -0500

    Update

--- uit-client_2.0-1_amd64/opt/uit-toolbox/powershell/backup-script.ps1
+++ uit-client_2.0-1_amd64/opt/uit-toolbox/powershell/backup-script.ps1
@@ -1,11 +1,13 @@
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
+Set-Variable -name "remoteHost" -Value ""
+Set-Variable -name "remoteUser" -Value ""
 Set-Variable -name "localSqlDir" -Value "C:\Users\caraschk\Desktop\00-sql-backups"
 Set-Variable -name "localImageDir" -Value "C:\Users\caraschk\Desktop\00-image-backups"
 Set-Variable -name "localImageZipFile" -Value "C:\Users\caraschk\Desktop\00-image-backups.zip"
 
 
-scp -r cameron@172.27.53.144:/opt/uit-toolbox/sql-backups/ $localSqlDir
-scp -r cameron@172.27.53.144:/opt/uit-toolbox/uit-web/inventory-images/ $localImageDir
+scp -r $remoteUser@$remoteHost\:/opt/uit-toolbox/sql-backups/ $localSqlDir
+scp -r $remoteUser@$remoteHost\:/opt/uit-toolbox/uit-web/inventory-images/ $localImageDir
 
 Compress-Archive -Path "$localImageDir\*" -DestinationPath "$localImageZipFile" -Force
