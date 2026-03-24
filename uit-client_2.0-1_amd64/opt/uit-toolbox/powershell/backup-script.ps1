acb6858463c4d147604b56ff8f9225b3049cbc05
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-03-24 10:38:40 -0500

    Update backup-script.ps1

--- uit-client_2.0-1_amd64/opt/uit-toolbox/powershell/backup-script.ps1
+++ uit-client_2.0-1_amd64/opt/uit-toolbox/powershell/backup-script.ps1
@@ -1,13 +1,26 @@
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Add-Type -AssemblyName System.Windows.Forms
+$dialogObj = New-Object System.Windows.Forms.FolderBrowserDialog
+$dialogObj.Description = "Select a folder to save the backups"
+$fileDialog = $dialogObj.ShowDialog()
+
+if ($fileDialog -eq [System.Windows.Forms.DialogResult]::OK -and 
+    -not [string]::IsNullOrWhiteSpace($dialogObj.SelectedPath)) {
+		Write-Host "Selected folder: $($dialogObj.SelectedPath)"
+} else {
+		Write-Host "No folder selected. Exiting."
+		exit
+}
 
 Set-Variable -name "remoteHost" -Value ""
 Set-Variable -name "remoteUser" -Value ""
-Set-Variable -name "localSqlDir" -Value "C:\Users\caraschk\Desktop\00-sql-backups"
-Set-Variable -name "localImageDir" -Value "C:\Users\caraschk\Desktop\00-image-backups"
-Set-Variable -name "localImageZipFile" -Value "C:\Users\caraschk\Desktop\00-image-backups.zip"
+Set-Variable -name "localDir" -Value $dialogObj.SelectedPath
+Set-Variable -name "localSqlDir" -Value "$($dialogObj.SelectedPath)\sql-backups"
+Set-Variable -name "localImageDir" -Value "$($dialogObj.SelectedPath)\image-backups"
+Set-Variable -name "localImageZipFile" -Value "$($dialogObj.SelectedPath)\image-backups.zip"
 
 
-scp -r $remoteUser@$remoteHost\:/opt/uit-toolbox/sql-backups/ $localSqlDir
-scp -r $remoteUser@$remoteHost\:/opt/uit-toolbox/uit-web/inventory-images/ $localImageDir
+scp -r ${remoteUser}@${remoteHost}:/opt/uit-toolbox/sql-backups/ $localSqlDir
+scp -r ${remoteUser}@${remoteHost}:/opt/uit-toolbox/uit-web/inventory-images/ $localImageDir
 
-Compress-Archive -Path "$localImageDir\*" -DestinationPath "$localImageZipFile" -Force
+Compress-Archive -Path "${localImageDir}\*" -DestinationPath "${localImageZipFile}" -Force
