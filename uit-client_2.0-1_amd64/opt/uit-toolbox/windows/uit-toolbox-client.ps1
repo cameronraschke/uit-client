768086d286994815c7c3bcf69137162ba9045396
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-04-03 15:34:20 -0500

    Update uit-toolbox-client.ps1

--- uit-client_2.0-1_amd64/opt/uit-toolbox/windows/uit-toolbox-client.ps1
+++ uit-client_2.0-1_amd64/opt/uit-toolbox/windows/uit-toolbox-client.ps1
@@ -3,8 +3,11 @@ $arr = @{
 'chassis_type' = (Get-CimInstance -Class Win32_ComputerSystem).ChassisSKUNumber
 'ad_domain' = (Get-CimInstance -Class Win32_ComputerSystem).Domain
 'ad_domain_joined' = (($null -ne $adDomain) -and ($adDomain -ne ""))
+'ad_domain_user' = (Get-CimInstance -Class Win32_ComputerSystem).DNSHostName
 'system_manufacturer' = (Get-CimInstance -Class Win32_ComputerSystem).Manufacturer
 'system_model' = (Get-CimInstance -Class Win32_ComputerSystem).Model
+'system_sku' = (Get-CimInstance -Class Win32_ComputerSystem).SystemSKUNumber
+'tpm_version' = (Get-WmiObject -Namespace "Root\CIMv2\Security\MicrosoftTpm" -Class Win32_Tpm | Select-Object -ExpandProperty SpecVersion) -split ", " | Select-Object -First 1
 'bios_version' = (Get-CimInstance -Class Win32_BIOS).SMBIOSBIOSVersion
 'os_name' = (Get-CimInstance -Class Win32_OperatingSystem).Caption
 'os_installed_at' = (Get-Date -Date ((Get-CimInstance -Class Win32_OperatingSystem).InstallDate)).ToString("yyyy-MM-dd'T'HH:mm:ssK")
@@ -15,12 +18,15 @@ $arr = @{
 'cpu_model' = (Get-CimInstance -Class Win32_Processor).Name
 'cpu_core_count' = (Get-CimInstance -Class Win32_Processor).NumberOfCores
 'cpu_thread_count' = (Get-CimInstance -Class Win32_Processor).NumberOfLogicalProcessors
+'disk_model' = (Get-CimInstance -Class Win32_DiskDrive | Select-Object -ExpandProperty Model | Select-Object -First 1)
+'disk_type' = (Get-Disk | Where-Object { ($_.DiskNumber -eq "0") } | Select-Object -ExpandProperty BusType).ToLower()
 'disk_size_kb' = (Get-CimInstance -Class Win32_DiskDrive | Measure-Object -Property Size -Sum).Sum / 1024
+'disk_free_space_kb' = (Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType = '3' AND Name = 'C:'" | Measure-Object -Property FreeSpace -Sum).Sum / 1024
 'ethernet_mac_addr' = (Get-CimInstance -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled } | Select-Object -ExpandProperty MACAddress | Select-Object -First 1)
 'wifi_mac_addr' = (Get-CimInstance -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -and $_.Description -match "Wireless" } | Select-Object -ExpandProperty MACAddress | Select-Object -First 1)
-'disk_model' = (Get-CimInstance -Class Win32_DiskDrive | Select-Object -ExpandProperty Model | Select-Object -First 1)
 # 'cpuTemp' = (Get-CimInstance -Namespace root\wmi -Class MSAcpi_ThermalZoneTemperature | Select-Object -ExpandProperty CurrentTemperature | Select-Object -First 1) / 10 - 273.15
 'battery_charge_percent' = (Get-CimInstance -Class Win32_Battery).EstimatedChargeRemaining
+'updated_from_windows' = $true
 }
 
 $httpBodyArr = @{}
