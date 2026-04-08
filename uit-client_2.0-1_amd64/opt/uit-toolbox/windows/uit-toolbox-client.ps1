1ea6ca838c5a07707a5c5da81dc50afa2514a2da
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-04-08 10:35:04 -0500

    Update uit-toolbox-client.ps1

--- uit-client_2.0-1_amd64/opt/uit-toolbox/windows/uit-toolbox-client.ps1
+++ uit-client_2.0-1_amd64/opt/uit-toolbox/windows/uit-toolbox-client.ps1
@@ -1,27 +1,45 @@
+Set-Variable -Name "computerInfoObj" -Value (Get-ComputerInfo)
+Set-Variable -Name "win32CompSysObj" -Value (Get-CimInstance -Class Win32_ComputerSystem)
+Set-Variable -Name "win32ComputerSystemProductObj" -Value (Get-CimInstance -Class Win32_ComputerSystemProduct)
+# Set-Variable -Name "win32BiosObj" -Value (Get-CimInstance -Class Win32_BIOS)
+# Set-Variable -Name "win32OperatingSystemObj" -Value (Get-CimInstance -Class Win32_OperatingSystem)
+Set-Variable -Name "win32MemoryObj" -Value (Get-CimInstance -Class Win32_PhysicalMemory)
+Set-Variable -Name "win32ProcessorObj" -Value (Get-CimInstance -Class Win32_Processor)
+Set-Variable -Name "win32DiskDriveObj" -Value (Get-CimInstance -Class Win32_DiskDrive)
+Set-Variable -Name "win32LogicalDiskObj" -Value (Get-CimInstance -Class Win32_LogicalDisk -Filter "DriveType = '3' AND Name = 'C:'")
+
 $arr = @{
-'system_serial' = (Get-CimInstance -Class Win32_ComputerSystemProduct).IdentifyingNumber
-'chassis_type' = (Get-CimInstance -Class Win32_ComputerSystem).ChassisSKUNumber
-'ad_domain' = (Get-CimInstance -Class Win32_ComputerSystem).Domain
-'ad_domain_joined' = (($null -ne $adDomain) -and ($adDomain -ne ""))
-'ad_domain_user' = (Get-CimInstance -Class Win32_ComputerSystem).DNSHostName
-'system_manufacturer' = (Get-CimInstance -Class Win32_ComputerSystem).Manufacturer
-'system_model' = (Get-CimInstance -Class Win32_ComputerSystem).Model
-'system_sku' = (Get-CimInstance -Class Win32_ComputerSystem).SystemSKUNumber
+'tagnumber' = $null
+'system_serial' = $win32ComputerSystemProductObj.IdentifyingNumber
+# 'system_serial' = $computerInfoObj.BiosSeralNumber
+'system_manufacturer' = $computerInfoObj.CsManufacturer
+'system_model' = $computerInfoObj.CsModel
+'system_sku' = $win32CompSysObj.SystemSKUNumber
+'chassis_type' = $computerInfoObj.CsPCSystemType
+'bios_version' = $computerInfoObj.BiosSMBIOSBIOSVersion
+'bios_release_date' = (Get-Date -Date $computerInfoObj.BiosReleaseDate).ToString("yyyy-MM-dd'T'HH:mm:ssK")
 'tpm_version' = (Get-WmiObject -Namespace "Root\CIMv2\Security\MicrosoftTpm" -Class Win32_Tpm | Select-Object -ExpandProperty SpecVersion) -split ", " | Select-Object -First 1
-'bios_version' = (Get-CimInstance -Class Win32_BIOS).SMBIOSBIOSVersion
-'os_name' = (Get-CimInstance -Class Win32_OperatingSystem).Caption
-'os_installed_at' = (Get-Date -Date ((Get-CimInstance -Class Win32_OperatingSystem).InstallDate)).ToString("yyyy-MM-dd'T'HH:mm:ssK")
-'os_version' = (Get-CimInstance -Class Win32_OperatingSystem).BuildNumber
-'os_ubr' = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name UBR).UBR
+'os_installed_at' = (Get-Date -Date ($computerInfoObj.OsInstallDate)).ToString("yyyy-MM-dd'T'HH:mm:ssK")
+'os_vendor' = $computerInfoObj.OSManufacturer
+'os_platform' = $computerInfoObj.OsType
+'os_architecture' = $computerInfoObj.OsArchitecture
+'os_name' = $computerInfoObj.OSName
+'os_version' = $computerInfoObj.OSVersion
+'windows_display_version' = $computerInfoObj.OSDisplayVersion
+'windows_build_number' = $computerInfoObj.OsBuildNumber
+'windows_ubr' = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name UBR).UBR
+'windows_bitlocker_enabled' = (Get-BitLockerVolume -MountPoint "C:").VolumeStatus -eq "FullyEncrypted"
+'ad_domain' = $computerInfoObj.Domain
+'ad_domain_user' = $win32CompSysObj.DNSHostName
 'memory_capacity_kb' = (Get-CimInstance -Class Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1024
-'memory_speed_mhz' = (Get-CimInstance -Class Win32_PhysicalMemory).Speed | Select-Object -First 1
-'cpu_model' = (Get-CimInstance -Class Win32_Processor).Name
-'cpu_core_count' = (Get-CimInstance -Class Win32_Processor).NumberOfCores
-'cpu_thread_count' = (Get-CimInstance -Class Win32_Processor).NumberOfLogicalProcessors
-'disk_model' = (Get-CimInstance -Class Win32_DiskDrive | Select-Object -ExpandProperty Model | Select-Object -First 1)
+'memory_speed_mhz' = $win32MemoryObj.Speed | Select-Object -First 1
+'cpu_model' = $win32ProcessorObj.Name
+'cpu_core_count' = $win32ProcessorObj.NumberOfCores
+'cpu_thread_count' = $win32ProcessorObj.NumberOfLogicalProcessors
+'disk_model' = ($win32DiskDriveObj | Select-Object -ExpandProperty Model | Select-Object -First 1)
 'disk_type' = (Get-Disk | Where-Object { ($_.DiskNumber -eq "0") } | Select-Object -ExpandProperty BusType).ToLower()
-'disk_size_kb' = (Get-CimInstance -Class Win32_DiskDrive | Measure-Object -Property Size -Sum).Sum / 1024
-'disk_free_space_kb' = (Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType = '3' AND Name = 'C:'" | Measure-Object -Property FreeSpace -Sum).Sum / 1024
+'disk_size_kb' = ($win32DiskDriveObj | Measure-Object -Property Size -Sum).Sum / 1024
+'disk_free_space_kb' = ($win32LogicalDiskObj | Measure-Object -Property FreeSpace -Sum).Sum / 1024
 'ethernet_mac_addr' = (Get-CimInstance -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled } | Select-Object -ExpandProperty MACAddress | Select-Object -First 1)
 'wifi_mac_addr' = (Get-CimInstance -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -and $_.Description -match "Wireless" } | Select-Object -ExpandProperty MACAddress | Select-Object -First 1)
 # 'cpuTemp' = (Get-CimInstance -Namespace root\wmi -Class MSAcpi_ThermalZoneTemperature | Select-Object -ExpandProperty CurrentTemperature | Select-Object -First 1) / 10 - 273.15
