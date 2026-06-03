f4b20489c8693f4dc98b8c26cf33e94de98014ff
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-06-03 15:10:44 -0500

    Update backup-script.ps1

--- uit-client_2.0-1_amd64/opt/uit-toolbox/windows/backup-script.ps1
+++ uit-client_2.0-1_amd64/opt/uit-toolbox/windows/backup-script.ps1
@@ -16,10 +16,11 @@ Set-Variable -name "remoteUser" -Value ""
 Set-Variable -name "localDir" -Value $dialogObj.SelectedPath
 Set-Variable -name "localSqlDir" -Value "$($dialogObj.SelectedPath)\sql-backups"
 Set-Variable -name "localImageDir" -Value "$($dialogObj.SelectedPath)\image-backups"
+Set-Variable -name "localMigratedImageDir" -Value "$($dialogObj.SelectedPath)\migrated-image-backups"
 Set-Variable -name "localImageZipFile" -Value "$($dialogObj.SelectedPath)\image-backups.zip"
-
 
 scp -r ${remoteUser}@${remoteHost}:/opt/uit-toolbox/sql-backups/ $localSqlDir
 scp -r ${remoteUser}@${remoteHost}:/opt/uit-toolbox/uit-web/inventory-images/ $localImageDir
+scp -r ${remoteUser}@${remoteHost}:/opt/inventory-images/ $localMigratedImageDir
 
-Compress-Archive -Path "${localImageDir}\*" -DestinationPath "${localImageZipFile}" -Force
+#Compress-Archive -Path "${localImageDir}\*" -DestinationPath "${localImageZipFile}" -Force
