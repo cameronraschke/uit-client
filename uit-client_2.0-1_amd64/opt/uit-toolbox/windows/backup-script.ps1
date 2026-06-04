9243d5c9586d5ddaccd74fd4a6b8088583b4c5a5
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-06-04 13:25:53 -0500

    Update backup-script.ps1

--- uit-client_2.0-1_amd64/opt/uit-toolbox/windows/backup-script.ps1
+++ uit-client_2.0-1_amd64/opt/uit-toolbox/windows/backup-script.ps1
@@ -11,13 +11,20 @@ if ($fileDialog -eq [System.Windows.Forms.DialogResult]::OK -and -not [string]::
 	exit
 }
 
+$currentDate = [DateTime]::Now
+$dateString = $currentDate.ToString("yyyy-MM-dd-HHmmss")
+
 Set-Variable -name "remoteHost" -Value ""
 Set-Variable -name "remoteUser" -Value ""
 Set-Variable -name "localDir" -Value $dialogObj.SelectedPath
-Set-Variable -name "localSqlDir" -Value "$($dialogObj.SelectedPath)\sql-backups"
-Set-Variable -name "localImageDir" -Value "$($dialogObj.SelectedPath)\image-backups"
-Set-Variable -name "localMigratedImageDir" -Value "$($dialogObj.SelectedPath)\migrated-image-backups"
-Set-Variable -name "localImageZipFile" -Value "$($dialogObj.SelectedPath)\image-backups.zip"
+Set-Variable -name "localSqlDir" -Value "$($dialogObj.SelectedPath)\sql-backups\$dateString"
+Set-Variable -name "localImageDir" -Value "$($dialogObj.SelectedPath)\image-backups\$dateString"
+Set-Variable -name "localMigratedImageDir" -Value "$($dialogObj.SelectedPath)\migrated-image-backups\$dateString"
+# Set-Variable -name "localImageZipFile" -Value "$($dialogObj.SelectedPath)\image-backups.zip"
+
+mkdir -Path $localSqlDir -Force
+mkdir -Path $localImageDir -Force
+mkdir -Path $localMigratedImageDir -Force
 
 scp -r ${remoteUser}@${remoteHost}:/opt/uit-toolbox/sql-backups/ $localSqlDir
 scp -r ${remoteUser}@${remoteHost}:/opt/uit-toolbox/uit-web/inventory-images/ $localImageDir
