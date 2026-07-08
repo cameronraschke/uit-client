0ba00842ef148562fabd2554817ef92f13262fed
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-07-08 12:34:01 -0500

    Update

--- uit-client_2.0-1_amd64/opt/uit-toolbox/windows/backup-script.ps1
+++ uit-client_2.0-1_amd64/opt/uit-toolbox/windows/backup-script.ps1
@@ -4,25 +4,38 @@ $currentDate = [DateTime]::Now
 $dateString = $currentDate.ToString("yyyy-MM-dd-HHmmss")
 
 $remoteHost = ""
+if ($remoteHost -eq "") {
+		Write-Host "Error: Remote host is not specified. Please set the remote host variable."
+		exit 1
+}
 $remoteUser = ""
+if ($remoteUser -eq "") {
+		Write-Host "Error: Remote user is not specified. Please set the remote user variable."
+		exit 1
+}
 $desktopDir = [Environment]::GetFolderPath("Desktop")
-$tmpDir = Join-Path "C:\Windows\Temp" "${dateString}-uit-backup"
+$tmpDir = Join-Path $env:TEMP "${dateString}-uit-backup"
 $pgDumpDir = Join-Path $tmpDir "sql-backups"
 $oldImagesDir = Join-Path $tmpDir "image-backups"
 $migratedImagesDir = Join-Path $tmpDir "migrated-image-backups"
-$outDir = Join-Path $desktopDir "00-uit-web-backups\${dateString}"
-$localImageZipFile = Join-Path $outDir "${dateString}-uit-backup.zip"
+$outFile = Join-Path $desktopDir "00-uit-web-backups\${dateString}-uit-backup.zip"
 
 mkdir -Path $tmpDir -Force
-mkdir -Path $outDir -Force
 mkdir -Path $pgDumpDir -Force
 mkdir -Path $oldImagesDir -Force
 mkdir -Path $migratedImagesDir -Force
 
-scp -r ${remoteUser}@${remoteHost}:/opt/uit-toolbox/sql-backups/ $pgDumpDir
-scp -r ${remoteUser}@${remoteHost}:/opt/uit-toolbox/uit-web/inventory-images/ $oldImagesDir
-scp -r ${remoteUser}@${remoteHost}:/opt/inventory_images/ $migratedImagesDir
+scp -r ${remoteUser}@${remoteHost}:/opt/uit-toolbox/sql-backups/* $pgDumpDir
+scp -r ${remoteUser}@${remoteHost}:/opt/uit-toolbox/uit-web/inventory-images/* $oldImagesDir
+scp -r ${remoteUser}@${remoteHost}:/opt/inventory_images/* $migratedImagesDir
 
-Compress-Archive -Path "${outDir}\*" -DestinationPath "${localImageZipFile}" -Force
+Compress-Archive -Path "${tmpDir}\*" -DestinationPath "${outFile}" -Force
 
-Remove-Item -Path $tmpDir -Recurse -Force
+if (Test-File $outFile) {
+	Write-Host "Backup completed successfully. Backup archive path: '${outFile}'"
+	if (Test-Path $tmpDir) {
+		Remove-Item -Path $tmpDir -Recurse -Force
+	}
+} else {
+	Write-Host "Backup failed. Temporary files at '${tmpDir}' are being retained."
+}
