e7a4a52fddc30af878f639c5ba37e2e6c52ba4e4
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-07-20 11:15:54 -0500

    Create SetupComplete.cmd

---
+++ uit-client_2.0-1_amd64/opt/uit-toolbox/windows/SetupComplete.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+:: C:\Windows\Setup\Scripts\SetupComplete.cmd
+
+:: TPM reset, re-initializes TPM on next boot
+powershell -Command "Clear-Tpm"
+
+:: Clear windows update identifiers
+net stop wuauserv
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" /v SusClientId /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" /v SusClientIdValidation /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" /v PingID /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" /v AccountDomainSid /f
+net start wuauserv
+
+:: Clear machine GUIDs
+reg delete "HKLM\SOFTWARE\Microsoft\Cryptography" /v MachineGuid /f
+reg delete "HKLM\SOFTWARE\Microsoft\SQMClient" /v MachineId /f
+
+:: Clear GPOs 
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\History" /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\State" /f
+
+:: Prepare 2023 secure boot CA
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecureBoot" /v "AvailableUpdates" /t REG_DWORD /d 0x5944 /f
+schtasks /run /tn "\Microsoft\Windows\PI\Secure-Boot-Update"
