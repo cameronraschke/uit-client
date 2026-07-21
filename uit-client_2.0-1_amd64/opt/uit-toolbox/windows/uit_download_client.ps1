2268437fb6b660761d9f53607514f68bb47c99b4
Cameron Raschke <110272354+cameronraschke@users.noreply.github.com>
2026-07-21 10:02:24 -0500

    Update

--- uit-client_2.0-1_amd64/opt/uit-toolbox/windows/uit_download_client.ps1
+++ uit-client_2.0-1_amd64/opt/uit-toolbox/windows/uit_download_client.ps1
@@ -1,5 +1,5 @@
 $desktopPath = [Environment]::GetFolderPath("Desktop")
 $clientPath = Join-Path -Path $desktopPath -ChildPath "uit-toolbox-client.ps1"
-curl -o $clientPath https://raw.githubusercontent.com/cameronraschke/uit-client/refs/heads/main/uit-client_2.0-1_amd64/opt/uit-toolbox/windows/uit-toolbox-client.ps1
+curl -o $clientPath https://raw.githubusercontent.com/cameronraschke/uit-client/refs/heads/main/uit-client_2.0-1_amd64/opt/uit-toolbox/windows/uit-toolbox-client/collect-info.ps1
 
 Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$clientPath`"" -Verb RunAs
